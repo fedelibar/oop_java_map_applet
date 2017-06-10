06c7b58319b850305b941c6e7dd9bd833577a9a5
Federico Lima <fedelibar@gmail.com>
2017-06-09 21:23:10 -0300

    Changed zoom and map provider due to block from google servers

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -6,6 +6,7 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 /** HelloWorld
@@ -46,9 +47,9 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		AbstractMapProvider provider = new OpenStreetMap.OpenStreetMapProvider();
 		// Set a zoom level
-		int zoomLevel = 10;
+		int zoomLevel = 12;
 		
 		if (offline) {
 			// If you are working offline, you need to use this provider 
@@ -65,7 +66,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 50, 50, 300, 450, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
@@ -76,6 +77,9 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 400, 50, 300, 450, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(-34.9f, -56.2f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -84,6 +88,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
